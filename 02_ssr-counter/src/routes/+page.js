9863d843dfab09fc9edcdbf6f996911ada0959e8
wass <wassim.filali@gmail.com>
2023-02-26 13:19:26 +0100

    server counter running

--- 02_ssr-counter/src/routes/+page.js
+++ 02_ssr-counter/src/routes/+page.js
@@ -1,8 +1,16 @@
+import { get_count, increment } from "../lib/shared";
+
 
 export const prerender = false
 export const ssr = true;
 export const csr = false;
 
 export function load({params}){
-    console.log("hello from home load()")
+    increment()
+    const current_count = get_count()
+    console.log(`hello from home load() counter = ${current_count}`)
+    
+    return{
+        counter: current_count
+    }
 }
--- 02_ssr-counter/src/routes/+page.svelte
+++ 02_ssr-counter/src/routes/+page.svelte
@@ -1,8 +1,20 @@
+<script>
+    export let data;
+</script>
+
 <h1>Svelte Kit Basics - Home Page</h1>
-<p>github project <a href="https://github.com/MicroWebStacks/svelte-examples/tree/main/01_basics" target="_blank" rel="nonreferrer">01_basics</a></p>
+<p>github project <a href="https://github.com/MicroWebStacks/svelte-examples/tree/main/02_ssr-counter" target="_blank" rel="nonreferrer">01_basics</a></p>
 
 <p>This page is rendered on the server</p>
 
+<p class="message">The counter value is {data.counter}</p>
 
 <style>
+    .message{
+        background-color: rgb(218, 218, 218);
+        border-radius: 5px;
+        width: fit-content;
+        padding: 10px;
+        box-shadow: inset 0 0 10px #a6a6a6;
+    }
 </style>
